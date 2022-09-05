c9850d1ee4e01916dc22f331ed26a8de50b7c239
Vivek Sharma <vivek@vybenetwork.com>
2022-09-04 22:08:50 -0700

    add implementation for transaction engine

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,9 @@ impl Config {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
 #[serde(rename_all = "lowercase")]
-enum TransactionType {
+pub enum TransactionType {
     Deposit,
     Withdrawal,
     Dispute,
@@ -38,23 +38,40 @@ enum TransactionType {
     Chargeback,
 }
 
+pub type ClientId = u16;
+pub type TransactionId = u32;
+pub type Amount = f32;
+
 #[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct Transaction {
+pub struct TransactionInput {
     #[serde(rename = "type")]
     kind: TransactionType,
-    client: u16,
-    tx: u32,
-    amount: f32,
+    client: ClientId,
+    tx: TransactionId,
+    amount: Option<Amount>,
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let transaction_engine = transaction_engine::TransactionEngine::new();
+    let mut transaction_engine = transaction_engine::TransactionEngine::new();
     let mut reader = csv::ReaderBuilder::new()
         .trim(csv::Trim::All)
         .from_path(config.input_path)?;
     for row_result in reader.deserialize() {
-        let transaction: Transaction = row_result?;
-        println!("{:?}", transaction);
+        let transaction: TransactionInput = row_result?;
+        if let Err(e) = transaction_engine.process_transaction(transaction) {
+            eprintln!("An error occurred when processing a transaction and it was skipped. We'll continue with next transactions. Error: {}", e);
+        }
+    }
+    println!("client, available, held, total, locked");
+    for (client_id, client_details) in transaction_engine.accounts {
+        println!(
+            "{:>6},{:>10.4},{:>5.4},{:>6.4},{:>7}",
+            client_id,
+            client_details.available,
+            client_details.held,
+            client_details.total,
+            client_details.locked
+        );
     }
     Ok(())
 }
--- src/transaction_engine.rs
+++ src/transaction_engine.rs
@@ -1,22 +1,310 @@
-use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Account {
-    client: u16,
-    available: f32,
-    held: f32,
-    total: f32,
-    locked: bool,
+use thiserror::Error;
+
+pub use crate::{Amount, ClientId, TransactionId};
+pub use crate::{TransactionInput, TransactionType};
+
+#[derive(Error, Debug, Clone)]
+pub enum TransactionProcessingError {
+    #[error("transaction can't be processed as account is locked")]
+    AccountLocked,
+
+    #[error("account not found")]
+    AccountNotFound,
+
+    #[error("transaction cannot be completed due to insufficient funds")]
+    InsufficientFunds,
+
+    #[error("amount value required to process the transaction of specified type")]
+    AmountValueNotFound,
+
+    #[error("provided transaction id not found")]
+    TransactionNotFound,
+
+    #[error("provided transaction id not found")]
+    AmountNotFoundOnTransactionToDispute,
+
+    #[error("cannot resolve a non disputed transaction")]
+    CannotResolveNonDisputedTransaction,
+}
+
+pub struct AccountDetails {
+    pub available: Amount,
+    pub held: Amount,
+    pub total: Amount,
+    pub locked: bool,
+}
+
+struct TransactionDetails {
+    kind: TransactionType,
+    client: ClientId,
+    amount: Option<Amount>,
+    is_disputed: bool,
 }
 
 pub struct TransactionEngine {
-    accounts: Vec<Account>,
+    // not putting client inside and using a hashmap as searching which would need to be
+    // done when processing every tx, would be an O(1)
+    // operation while in a simple vec, it would take longer
+    pub accounts: HashMap<ClientId, AccountDetails>,
+    transactions: HashMap<TransactionId, TransactionDetails>,
 }
 
 impl TransactionEngine {
     pub fn new() -> TransactionEngine {
         TransactionEngine {
-            accounts: Vec::new(),
+            accounts: HashMap::new(),
+            transactions: HashMap::new(),
+        }
+    }
+
+    pub fn process_transaction(
+        &mut self,
+        transaction: TransactionInput,
+    ) -> Result<(), TransactionProcessingError> {
+        let previous_account_data = self.accounts.get(&transaction.client);
+        // if the account is locked, no transaction is allowed on it
+        if let Some(a) = previous_account_data {
+            if a.locked {
+                return Err(TransactionProcessingError::AccountLocked.into());
+            }
+        }
+
+        match transaction.kind {
+            TransactionType::Deposit => {
+                if let Some(amount) = transaction.amount {
+                    return self.process_deposit_transaction(
+                        transaction.tx,
+                        transaction.client,
+                        amount,
+                    );
+                } else {
+                    return Err(TransactionProcessingError::AmountValueNotFound.into());
+                };
+            }
+            TransactionType::Withdrawal => {
+                if let Some(amount) = transaction.amount {
+                    return self.process_withdrawal_transaction(
+                        transaction.tx,
+                        transaction.client,
+                        amount,
+                    );
+                } else {
+                    return Err(TransactionProcessingError::AmountValueNotFound.into());
+                };
+            }
+            TransactionType::Dispute => return self.process_dispute_transaction(transaction.tx),
+            TransactionType::Resolve => return self.process_resolve_transaction(transaction.tx),
+            TransactionType::Chargeback => {
+                return self.process_chargeback_transaction(transaction.tx)
+            }
+        }
+    }
+
+    fn process_deposit_transaction(
+        &mut self,
+        transaction_id: TransactionId,
+        client_id: u16,
+        amount: f32,
+    ) -> Result<(), TransactionProcessingError> {
+        let previous_account_data = self.accounts.entry(client_id).or_insert(AccountDetails {
+            available: 0.0,
+            total: 0.0,
+            held: 0.0,
+            locked: false,
+        });
+
+        *previous_account_data = AccountDetails {
+            available: previous_account_data.available + amount,
+            total: previous_account_data.total + amount,
+            held: previous_account_data.held,
+            locked: previous_account_data.locked,
+        };
+        self.transactions.insert(
+            transaction_id,
+            TransactionDetails {
+                kind: TransactionType::Deposit,
+                client: client_id,
+                amount: Some(amount),
+                is_disputed: false,
+            },
+        );
+        return Ok(());
+    }
+
+    fn process_withdrawal_transaction(
+        &mut self,
+        transaction_id: TransactionId,
+        client_id: ClientId,
+        amount: f32,
+    ) -> Result<(), TransactionProcessingError> {
+        let previous_account_data = self.accounts.get_mut(&client_id);
+        match previous_account_data {
+            Some(account) => {
+                if account.available > amount {
+                    *account = AccountDetails {
+                        available: account.available - amount,
+                        total: account.total - amount,
+                        held: account.held,
+                        locked: account.locked,
+                    };
+                    self.transactions.insert(
+                        transaction_id,
+                        TransactionDetails {
+                            kind: TransactionType::Deposit,
+                            client: client_id,
+                            amount: Some(amount),
+                            is_disputed: false,
+                        },
+                    );
+                    return Ok(());
+                } else {
+                    return Err(TransactionProcessingError::InsufficientFunds);
+                }
+            }
+            None => return Err(TransactionProcessingError::AccountNotFound),
+        }
+    }
+
+    fn process_dispute_transaction(
+        &mut self,
+        transaction_id: TransactionId,
+    ) -> Result<(), TransactionProcessingError> {
+        let existing_transaction_details = self.transactions.get_mut(&transaction_id);
+        match existing_transaction_details {
+            Some(t) => match t.amount {
+                Some(amount) => {
+                    let account_details = self.accounts.get_mut(&t.client);
+                    match account_details {
+                        Some(a) => {
+                            *a = AccountDetails {
+                                available: a.available - amount,
+                                total: a.total,
+                                held: a.held + amount,
+                                locked: a.locked,
+                            };
+
+                            *t = TransactionDetails {
+                                kind: t.kind,
+                                client: t.client,
+                                amount: t.amount,
+                                is_disputed: true,
+                            };
+                            Ok(())
+                        }
+                        None => return Err(TransactionProcessingError::AccountNotFound),
+                    }
+                }
+                None => {
+                    return Err(
+                        TransactionProcessingError::AmountNotFoundOnTransactionToDispute.into(),
+                    );
+                }
+            },
+            None => {
+                return Err(TransactionProcessingError::TransactionNotFound.into());
+            }
+        }
+    }
+
+    fn process_resolve_transaction(
+        &mut self,
+        transaction_id: TransactionId,
+    ) -> Result<(), TransactionProcessingError> {
+        let existing_transaction_details = self.transactions.get_mut(&transaction_id);
+        match existing_transaction_details {
+            Some(t) => {
+                if t.is_disputed {
+                    match t.amount {
+                        Some(amount) => {
+                            let account_details = self.accounts.get_mut(&t.client);
+                            match account_details {
+                                Some(a) => {
+                                    *a = AccountDetails {
+                                        available: a.available + amount,
+                                        total: a.total,
+                                        held: a.held - amount,
+                                        locked: a.locked,
+                                    };
+
+                                    *t = TransactionDetails {
+                                        kind: t.kind,
+                                        client: t.client,
+                                        amount: t.amount,
+                                        is_disputed: false,
+                                    };
+                                    Ok(())
+                                }
+                                None => return Err(TransactionProcessingError::AccountNotFound),
+                            }
+                        }
+                        None => {
+                            return Err(
+                                TransactionProcessingError::AmountNotFoundOnTransactionToDispute
+                                    .into(),
+                            );
+                        }
+                    }
+                } else {
+                    return Err(
+                        TransactionProcessingError::CannotResolveNonDisputedTransaction.into(),
+                    );
+                }
+            }
+            None => {
+                return Err(TransactionProcessingError::TransactionNotFound.into());
+            }
+        }
+    }
+
+    fn process_chargeback_transaction(
+        &mut self,
+        transaction_id: TransactionId,
+    ) -> Result<(), TransactionProcessingError> {
+        let existing_transaction_details = self.transactions.get_mut(&transaction_id);
+        match existing_transaction_details {
+            Some(t) => {
+                if t.is_disputed {
+                    match t.amount {
+                        Some(amount) => {
+                            let account_details = self.accounts.get_mut(&t.client);
+                            match account_details {
+                                Some(a) => {
+                                    *a = AccountDetails {
+                                        available: a.available,
+                                        total: a.total - amount,
+                                        held: a.held - amount,
+                                        locked: true,
+                                    };
+
+                                    *t = TransactionDetails {
+                                        kind: t.kind,
+                                        client: t.client,
+                                        amount: t.amount,
+                                        is_disputed: false,
+                                    };
+                                    Ok(())
+                                }
+                                None => return Err(TransactionProcessingError::AccountNotFound),
+                            }
+                        }
+                        None => {
+                            return Err(
+                                TransactionProcessingError::AmountNotFoundOnTransactionToDispute
+                                    .into(),
+                            );
+                        }
+                    }
+                } else {
+                    return Err(
+                        TransactionProcessingError::CannotResolveNonDisputedTransaction.into(),
+                    );
+                }
+            }
+            None => {
+                return Err(TransactionProcessingError::TransactionNotFound.into());
+            }
         }
     }
 }
